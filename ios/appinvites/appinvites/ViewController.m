34704f6de3247325c19701368a4fcc77ac64cd3a
Gus Class <class@google.com>
2015-04-06 16:10:25 -0700

    Apache headers
    Change-Id: I9e2a7b21a47d9057701836827ef239545ef9d4d1

--- ios/appinvites/appinvites/ViewController.m
+++ ios/appinvites/appinvites/ViewController.m
@@ -2,9 +2,17 @@
 //  ViewController.m
 //  appinvites
 //
-//  Created by Gus Class on 4/3/15.
-//  Copyright (c) 2015 gusclass. All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 #import "ViewController.h"
 #import <GPPInvite/GPPInvite.h>
@@ -20,7 +28,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  // Configure the sign-in button look/feel
+  // TODO(developer) Configure the sign-in button look/feel
   [GIDSignIn sharedInstance].delegate = self;
 
   [[GIDSignIn sharedInstance] signInSilently];
@@ -98,11 +106,5 @@ didDisconnectWithUser:(GIDGoogleUser *)user
     self.inviteButton.enabled = YES;
   }
 }
-
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
-
 
 @end
