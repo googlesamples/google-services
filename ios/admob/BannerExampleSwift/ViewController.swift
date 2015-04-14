9e80c1a96e3e040718bb515cb481deee6cd076b2
Ibrahim Ulukaya <ulukaya@google.com>
2015-04-14 16:14:05 -0400

    Merged version of Admob iOS Quickstart
    Change-Id: Ifa703b735f1053ab867284a97b9225ca0fba79d7

---
+++ ios/admob/BannerExampleSwift/ViewController.swift
@@ -0,0 +1,44 @@
+//
+// Copyright (c) 2015 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//  ViewController.swift
+//  BannerExample
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var bannerView: GADBannerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Replace this ad unit ID with your own ad unit ID.
+        self.bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        self.bannerView.rootViewController = self
+
+        let request = GADRequest();
+        // Requests test ads on devices you specify. Your test device ID is printed to the console when
+        // an ad request is made. GADBannerView automatically returns test ads when running on a
+        // simulator.
+        // Replace this device ID with your own test device ID.
+        request.testDevices = [
+        "2077ef9a63d2b398840261c8221a0c9a"
+        ]
+        self.bannerView.loadRequest(request)
+    }
+
+}
+
---
+++ ios/admob/README.md
@@ -0,0 +1,26 @@
+iOS Banner Example
+==================
+This is a minimal example showing how to place a banner ad into an iOS
+application.
+
+This example does not come bundled with the iOS Google Mobile Ads SDK, but is
+compatible with Cocoapods and includes a Podfile. We recommend using Cocoapods
+to reference the SDK. You can do this by running:
+
+`pod install`
+
+If you don't have Cocoapods installed, visit
+http://guides.cocoapods.org/using/getting-started.html#toc_3 to get started.
+
+After running pod update, Cocoapods creates a .xcworkspace that you can open
+and run.
+
+Replace Your Ad Unit ID
+-----------------------
+This example comes with a preconfigured ad unit ID. You should create an ad
+unit ID on http://apps.admob.com and use that ad unit ID in your own apps.
+
+Additional Resources
+--------------------
+* [Developer documentation](https://developers.google.com/mobile-ads-sdk)
+* [Developer forum](groups.google.com/group/google-admob-ads-sdk)
