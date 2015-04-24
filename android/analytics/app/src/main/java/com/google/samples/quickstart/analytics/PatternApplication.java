2f0e6544d98698257e1c9e79c114346a0eec2f9a
Sam Thorogood <thorogood@google.com>
2015-04-24 16:19:43 +1000

    analytics quickstart for android
    Change-Id: I03e76451b0a4522ff3d520f45614e85485100769

---
+++ android/analytics/app/src/main/java/com/google/samples/quickstart/analytics/PatternApplication.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.quickstart.analytics;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Logger;
+import com.google.android.gms.analytics.Tracker;
+
+/**
+ * This is a subclass of {@link Application} used to provide shared objects for this app, such as
+ * the {@link Tracker}.
+ */
+public class PatternApplication extends Application {
+  private Tracker mTracker;
+
+  /**
+   * Gets the default {@link Tracker} for this {@link Application}.
+   * @return tracker
+   */
+  synchronized public Tracker getDefaultTracker() {
+    if (mTracker == null) {
+      GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+      analytics.getLogger().setLogLevel(Logger.LogLevel.VERBOSE);
+
+      // TODO(thorogood): Explain this with <API 14.
+      analytics.enableAutoActivityReports(this);
+
+      mTracker = analytics.newTracker(R.xml.tracker);
+    }
+    return mTracker;
+  }
+}
