cd7dddaa3d0c5abb76ebeab63223a389aadbdae6
Ibrahim Ulukaya <ulukaya@google.com>
2015-04-24 13:10:22 -0400

    Adding GMP context to Admob iOS quickstart
    Change-Id: Ifeffc69906302168d1d35e1aa47ae04669fb8337

--- ios/admob/BannerExampleSwift/AppDelegate.swift
+++ ios/admob/BannerExampleSwift/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        GHIContext.sharedInstance().configure()
         return true
     }
 
