85c42b2f909368cedc2cf9187f94aff86995a7d9
Eric Gilmore <egilmore@google.com>
2015-10-16 15:21:39 -0700

    Clarifying how to get Sender ID value from generated config file.

--- android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/RegistrationIntentService.java
+++ android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/RegistrationIntentService.java
@@ -48,6 +48,8 @@ public class RegistrationIntentService extends IntentService {
             // are local.
             // [START get_token]
             InstanceID instanceID = InstanceID.getInstance(this);
+            // SenderID value is typically derived from google-services.json.
+            // See https://developers.google.com/cloud-messaging/android/start for details on this file.
             String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
                     GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
             // [END get_token]
