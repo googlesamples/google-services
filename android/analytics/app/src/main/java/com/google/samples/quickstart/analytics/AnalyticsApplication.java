5fa2a17e8ca3e5632e649f21bd32c79276d1581f
Tiem Song <tiem@google.com>
2016-02-10 16:26:49 -0800

    Fix warnings, remove unused imports, replace deprecated method.

--- android/analytics/app/src/main/java/com/google/samples/quickstart/analytics/AnalyticsApplication.java
+++ android/analytics/app/src/main/java/com/google/samples/quickstart/analytics/AnalyticsApplication.java
@@ -19,7 +19,6 @@ package com.google.samples.quickstart.analytics;
 import android.app.Application;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Logger;
 import com.google.android.gms.analytics.Tracker;
 
 /**
