da9cbef43d2c51fa60828adb4f74227edc2122b7
Ibrahim Ulukaya <ulukaya@gmail.com>
2017-07-13 15:43:36 -0400

    Update test.sh

--- ios/test.sh
+++ ios/test.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
-set -o pipefail && xcodebuild \
+set -eo pipefail
+
+EXIT_STATUS=0
+
+(xcodebuild \
   -workspace ios/${SAMPLE}/${SAMPLE}Example.xcworkspace \
   -scheme ${SAMPLE}Example \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 7' \
+  -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 7' \
   build \
   test \
   ONLY_ACTIVE_ARCH=YES \
-  CODE_SIGNING_REQUIRED=NO\
-  
-RESULT=$?
-if [ $RESULT == 65 ]; then
-  echo "xcodebuild exited with 65, retrying"
-  set -o pipefail && xcodebuild \
-    -workspace ios/${SAMPLE}/${SAMPLE}Example.xcworkspace \
-    -scheme ${SAMPLE}Example \
-    -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPhone 7' \
-    build \
-    test \
-    ONLY_ACTIVE_ARCH=YES \
-    CODE_SIGNING_REQUIRED=NO\
-    | xcpretty
-else
-  exit $RESULT
-fi
+  CODE_SIGNING_REQUIRED=NO \
+  | xcpretty) || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
