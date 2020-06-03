6434e6d2b9bb78b27728d148bb8ee9153c51c687
Adam <caller9@users.noreply.github.com>
2020-06-03 15:29:24 -0500

    Create empty project

---
+++ android/canonical/app/src/main/java/com/google/samples/quickstart/canonical/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.google.samples.quickstart.canonical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
